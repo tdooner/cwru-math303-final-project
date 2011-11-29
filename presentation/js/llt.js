c2a6614ed1f00a1da55fed7f7cfa324aa96d8ce9
Brian Stack <bis12@case.edu>
2011-11-29 00:22:32 -0500

    Add sieve and update llt in presentation

--- presentation/js/llt.js
+++ presentation/js/llt.js
@@ -5,20 +5,21 @@ var LLT = function(p) {
         s = s.square().subtract(2).remainder(M);
     }
     if (s.compare(0) == 0) { // if (s == 0) {
+        $("#LLTPrime").html(M.toString());
         return true
     } else {                 // } else {
         return false
     }                        // }
 }
-var RunLLT = function(i,max) {
-    a = TimeLLT(i)
+var RunLLT = function(i, sorted) {
+    a = TimeLLT(sorted[i])
     if (a["result"] == true) {
-        $("#llt-"+i).addClass("good");
+        $("#llt-"+sorted[i]).addClass("good");
     } else {
-        $("#llt-"+i).addClass("bad");
+        $("#llt-"+sorted[i]).addClass("bad");
     }
-    if (i < max) {
-        setTimeout("RunLLT("+(i+2)+","+max+")", 5);
+    if (i < sorted.length) {
+        setTimeout(function(){RunLLT(i+1,sorted);}, 5);
     }
 }
 var TimeLLT = function(i) {
@@ -31,3 +32,39 @@ var TimeLLT = function(i) {
        return {"result": false, "elapsed": after}
     }
 }
+
+var Sieve = function(max) {
+    var valid = {};
+    /* 
+     * Fill valid with odd integers between 3 and max,
+     * these will be the candidate primes.
+     */
+    for (var i = 3; i < max; i += 2){
+        valid[i] = true;
+    }
+    /*
+     * Now go from 3 to max/2 and remove multiples 
+     * from the valid set, the remaining ones will be prime.
+     */ 
+    for (var i = 3; i < max / 2; i += 2){
+        var j = i * 2;
+        while (j <= max){
+            $("#siv-"+j).addClass("bad");
+            delete valid[j];
+            j = j + i;
+        }
+    }
+    return valid;
+}
+
+var Generate = function(max_prime){
+    $("#siv-2").addClass("good");
+    var valid = Sieve(max_prime);
+    for (var i in valid){ 
+        $("#siv-"+i).addClass("good");
+        $("#LLTBox").append("<span id='llt-"+i+"'>"+i+"</span> ");
+    }
+    $("#llt-2").addClass("bad");
+    return valid;
+}
+
